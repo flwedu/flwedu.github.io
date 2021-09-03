a18a9c2f173b589b7100bf507f088470df6c0bc3
flwedu <edutraquino@gmail.com>
2021-09-03 20:17:54 -0300

    cards are supplied from json

---
+++ index.js
@@ -0,0 +1,21 @@
+// Request projects to array
+var dataLoader = new XMLHttpRequest();
+dataLoader.overrideMimeType("application/json");
+dataLoader.open("GET", "projects.json", true);
+dataLoader.onreadystatechange = function () {
+    if (dataLoader.readyState === 4 && dataLoader.status == 200) {
+        var response = JSON.parse(dataLoader.responseText);
+        render(response);
+    }
+};
+dataLoader.send();
+// Rendering projects list
+var render = function (list) {
+    var projectsList = document.querySelector(".projectsList");
+    list.forEach(function (project) {
+        var projectCard = document.createElement("div");
+        projectCard.classList.add("card");
+        projectCard.innerHTML = "\n    <img src=\"https://i.imgur.com/H3TCZef.png\" class=\"card-img-top\" />\n    <div class=\"card-body\">\n      <h5 class=\"card-title\">" + project.title + "</h5>\n      <p class=\"card-text\">\n        " + project.description + "\n      </p>\n      <a href=" + project.applicationLink + " class=\"btn btn-light\"\n        >Visite App</a\n      >\n      <a href=" + project.githubLink + " class=\"btn btn-light\"\n        >Github</a\n      >\n    </div>\n  ";
+        projectsList.appendChild(projectCard);
+    });
+};
---
+++ index.ts
@@ -0,0 +1,46 @@
+// Request projects to array
+const dataLoader = new XMLHttpRequest();
+dataLoader.overrideMimeType("application/json");
+dataLoader.open("GET", "projects.json", true);
+dataLoader.onreadystatechange = function () {
+  if (dataLoader.readyState === 4 && dataLoader.status == 200) {
+    let response = JSON.parse(dataLoader.responseText);
+    render(response);
+  }
+}
+
+dataLoader.send();
+
+interface Project {
+  title: string,
+  description: string,
+  applicationLink: string,
+  githubLink: string
+}
+
+// Rendering projects list
+var render = (list: any) => {
+  let projectsList = document.querySelector(".projectsList");
+  list.forEach(project => {
+    let projectCard = document.createElement("div");
+    projectCard.classList.add("card");
+
+    projectCard.innerHTML = `
+    <img src="https://i.imgur.com/H3TCZef.png" class="card-img-top" />
+    <div class="card-body">
+      <h5 class="card-title">${project.title}</h5>
+      <p class="card-text">
+        ${project.description}
+      </p>
+      <a href=${project.applicationLink} class="btn btn-light"
+        >Visite App</a
+      >
+      <a href=${project.githubLink} class="btn btn-light"
+        >Github</a
+      >
+    </div>
+  `
+
+    projectsList.appendChild(projectCard);
+  });
+}
