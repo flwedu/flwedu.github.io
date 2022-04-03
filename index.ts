a5032588121991f884cb149147ddb3356620e243
Eduardo Aquino <edutraquino@gmail.com>
2022-04-03 12:09:21 -0300

    refactor: logic of render list

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-// Request projects to array
-fetch("projects.json").then(res => res.json()).then(res => render(res)).catch(error => alert('Error loading projects list'));
-
 interface Project {
   title: string,
   description: string,
@@ -9,34 +6,52 @@ interface Project {
   githubLink: string
 }
 
-// Rendering projects list
-const render = (list: any) => {
-  let projectsList = document.querySelector("#projectsList");
-  list.forEach((project: Project) => {
-    projectsList.appendChild(createCard(project));
+interface TechImage {
+  src: string,
+  alt: string
+}
+
+// Rendering lists
+const renderProjects = (list: Project[]) => {
+  const projectsListEl = document.querySelector<HTMLDivElement>("#projectsList");
+  list.forEach((item) => {
+    const cardEl = document.createElement("div");
+    cardEl.classList.add("card");
+    cardEl.innerHTML = generateCardInnerHtml(item);
+
+    projectsListEl.appendChild(cardEl);
   });
 }
 
-const createCard = (project: Project) => {
-  const newCard = document.createElement("div");
-  newCard.classList.add("card");
-  newCard.innerHTML = generateInnerHtml(project);
-
-  return newCard;
+const renderTechnologies = (list: TechImage[]) => {
+  const techListEl = document.querySelector<HTMLDivElement>("#techList");
+  list.forEach((item) => {
+    const el = document.createElement("img");
+    el.src = item.src;
+    el.alt = item.alt;
+    techListEl.appendChild(el);
+  })
 }
 
-const generateInnerHtml = (project: Project) => {
+const generateCardInnerHtml = (project: Project) => {
   return `
   <div class="col-12">
     <img src=${project.imgUrl} class="card-img-top" />
     <div class="card-body">
-      <h5 class="card-title">${project.title}</h5>
-      <p class="card-text">
-        ${project.description}
-      </p>
+    <h5 class="card-title">${project.title}</h5>
+    <p class="card-text">
+    ${project.description}
+    </p>
       ${project.applicationLink ? "<a href='" + project.applicationLink + "' target='_blank' class='btn btn-light'>Visite App</a>" : ""}
       <a href=${project.githubLink} target="_blank" class="btn btn-light">Github</a>
-    </div>
-    </div>
-  `
+      </div>
+      </div>
+      `
 }
+
+const loadFromJson = (filePath: string, cb: any) => {
+  fetch(filePath).then(res => res.json()).then(cb).catch(console.error);
+}
+
+loadFromJson("./res/projects.json", renderProjects);
+loadFromJson("./res/technologies.json", renderTechnologies);
