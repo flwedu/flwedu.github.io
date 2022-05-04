72c16363d006ac32d72fa2d2c93ab35cbdcb9dcd
Eduardo Aquino <edutraquino@gmail.com>
2022-05-04 19:00:43 -0300

    set styles to technologies list

--- src/sections/Technologies.tsx
+++ src/sections/Technologies.tsx
@@ -1,6 +1,6 @@
 export function Technologies() {
   return (
-    <section>
+    <section id="technologies">
       <h1>Tecnologias</h1>
       <p>
         Aprendi Java e Orientação a Objetos durante a faculdade, mas me
@@ -9,6 +9,24 @@ export function Technologies() {
         organizados e fácil de manter, como Clean Code, Clean Architecture, DDD
         e TDD. Tecnologias de interesse:
       </p>
+      <div className="grid text-center mt-2 text-base sm:grid-cols-2 md:text-xl text-mainPurple dark:text-slate-300">
+        <ul>
+          <li>Javascript</li>
+          <li>Node.js</li>
+          <li>Typescript</li>
+          <li>React.js</li>
+          <li>HTML</li>
+          <li>CSS</li>
+        </ul>
+        <ul>
+          <li>Java & Spring</li>
+          <li>GIT & Github</li>
+          <li>Maven & Gradle</li>
+          <li>JUnit</li>
+          <li>GraphQL</li>
+          <li>API Rest</li>
+        </ul>
+      </div>
     </section>
   );
 }
