c50e29ec8ae28ff2cd7868f646a7c901df32244f
Eduardo Aquino <edutraquino@gmail.com>
2022-05-04 19:17:00 -0300

    adjust darkmode button size

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -9,10 +9,10 @@ export function DarkModeButton() {
 
   return (
     <div
-      className="sticky bottom-2 self-end rounded-full w-9 bg-slate-900 dark:bg-slate-100"
+      className="sticky bottom-2 self-end rounded-full w-[48px] bg-slate-900 dark:bg-slate-100"
       onClick={toggleDarkMode}
     >
-      <Alien size={36} className="text-slate-100 dark:text-slate-900" />
+      <Alien size={48} className="text-slate-100 dark:text-slate-900" />
     </div>
   );
 }
