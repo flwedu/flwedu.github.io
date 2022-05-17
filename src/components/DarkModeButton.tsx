a3daa2c5d11cec57bdec426fb0ae5099bfee528d
Eduardo Aquino <edutraquino@gmail.com>
2022-05-17 10:34:18 -0300

    expand the button to show help text about function

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -1,15 +1,26 @@
 import { Alien } from "phosphor-react";
 import useDarkModeContext from "../contexts/consumers/darkmode-consumer";
+import useTextContext from "../contexts/consumers/text-context-consumer";
 
 export default function DarkModeButton() {
-  const { toggleFn } = useDarkModeContext();
+  const { darkmode, toggleFn } = useDarkModeContext();
+  const { text } = useTextContext();
+
+  function getSpanText(darkmode: boolean) {
+    return darkmode ? text.buttons.toLightMode : text.buttons.toDarkMode;
+  }
 
   return (
-    <div
-      className="fixed bottom-[1.5rem] right-4 rounded-full h-16 w-16 bg-slate-900 dark:bg-slate-100"
-      onClick={toggleFn}
-    >
-      <Alien size={64} className="text-slate-100 dark:text-slate-900" />
+    <div className="fixed bottom-[1.5rem] right-4">
+      <button
+        className="px-2 rounded-full h-16 bg-slate-900 dark:bg-slate-100 group flex items-center"
+        onClick={toggleFn}
+      >
+        <Alien className="text-slate-100 dark:text-slate-900 w-12 h-12" />
+        <span className="text-slate-100 dark:text-slate-900 max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-in-out">
+          {getSpanText(darkmode)}
+        </span>
+      </button>
     </div>
   );
 }
--- src/components/LanguageButton.tsx
+++ src/components/LanguageButton.tsx
@@ -1,20 +1,25 @@
 import useTextContext from "../contexts/consumers/text-context-consumer";
 
 export default function LanguageButton() {
-  const { toggleFn, language } = useTextContext();
+  const { toggleFn, language, text } = useTextContext();
 
   function handleClick() {
     toggleFn();
   }
 
   return (
-    <div
-      className="fixed bottom-[6rem] right-4 rounded-full h-16 w-16 bg-slate-900 dark:bg-slate-100 flex justify-center items-center"
-      onClick={handleClick}
-    >
-      <span className="text-5xl text-center items-center">
-        {language == "en" ? "🇺🇸" : "🇧🇷"}
-      </span>
+    <div className="fixed bottom-[6rem] right-4">
+      <button
+        className="px-2 rounded-full h-16 bg-slate-900 dark:bg-slate-100 group flex items-center"
+        onClick={handleClick}
+      >
+        <span className="text-slate-100 dark:text-slate-900 text-5xl text-center items-center">
+          {text.emoji}
+        </span>
+        <span className="text-slate-100 dark:text-slate-900 max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-in-out">
+          {text.buttons.language}
+        </span>
+      </button>
     </div>
   );
 }
