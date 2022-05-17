470f073d66e5e979a3efd1849a631f1e06eb789f
Eduardo Aquino <edutraquino@gmail.com>
2022-05-17 11:41:11 -0300

    feat: show country flags (svg)

--- src/components/LanguageButton.tsx
+++ src/components/LanguageButton.tsx
@@ -1,4 +1,6 @@
 import useTextContext from "../contexts/consumers/text-context-consumer";
+import brazilFlag from "../../static/brazil.svg";
+import usFlag from "../../static/us.svg";
 
 export default function LanguageButton() {
   const { toggleFn, language, text } = useTextContext();
@@ -7,15 +9,21 @@ export default function LanguageButton() {
     toggleFn();
   }
 
+  function getImageUrl(language: string) {
+    return language == "pt-br" ? brazilFlag : usFlag;
+  }
+
   return (
     <div className="fixed bottom-[6rem] right-4">
       <button
         className="px-2 rounded-full h-16 bg-slate-900 dark:bg-slate-100 group flex items-center"
         onClick={handleClick}
       >
-        <span className="text-slate-100 dark:text-slate-900 text-5xl text-center items-center">
-          {text.emoji}
-        </span>
+        <img
+          src={getImageUrl(language)}
+          alt="Selected language country flag"
+          className="w-12 object-cover rounded-full items-center"
+        ></img>
         <span className="text-slate-100 dark:text-slate-900 max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-in-out">
           {text.buttons.language}
         </span>
